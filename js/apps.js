ab8f7db6ed37b7a29eb98b1afe5c240531ba0b0d
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-03-24 00:15:22 -0700

    Commit 1 - I.A.

---
+++ js/apps.js
@@ -0,0 +1,41 @@
+console.log('JavaScript weekend REPS!');
+
+// I. Variable & Datatypes
+    // A. Q + A
+        // 1. We assign a value by using const or let 
+        // and the assignment operator (=)
+
+        // 2. If we assigned a value using let we can change 
+        // the variable by "assigning" it a new value. 
+        // However if we used const then we must "mutate" 
+        // the variable.
+
+        // 3. let name = "Jerry";
+        // (to change the value of name)
+        // name = "Sharon";
+
+        // 4. declare, assign, and define are all referring 
+        // to the process of writing some code (assigning 
+        // value to a variable or declaring a function) so 
+        // it can be invoked or called upon throughout the code.
+
+        // 5. pseudocoding is where you write out the code 
+        // you want to put together in plain english first.
+        // It is an important step as it helps to 
+        // organize and set up your code. 
+
+        // 6. I think 50% of thinking about how you're going
+        // to solve a problem is sufficient. It's important
+        // to make your goals clear, and I believe in a good
+        // plan. Planning allows you to be open-minded and
+        // organized. The other 50% of the time should be 
+        // spent on executing your plan or the multiple ideas
+        // you thought of.
+
+        
+
+
+
+    
+
+
